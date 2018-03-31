9060224c8ffa4a918f1f762df3add934401d6e06
qingwei.li <cinwell.li@gmail.com>
2018-03-31 11:39:58 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+
+const fastify = require('fastify')();
+const md5 = require('md5');
+const COS = require('cos-nodejs-sdk-v5');
+
+const cos = new COS({
+  Bucket: process.env.BUCKET,
+  SecretId: process.env.SECRETID,
+  SecretKey: process.env.SECRETKEY
+});
+
+// Body limit: 256KB
+fastify.register(require('fastify-formbody'), { bodyLimit: 1024 * 256 });
+
+fastify.get('/', (request, reply) => {
+  reply.send(
+    `Text hosting service\n\nUsage:\n  curl -d text='any words' https://text.cinwell.com\n`
+  );
+});
+
+fastify.post('/', (req, reply) => {
+  const { text } = req.body;
+  const hash = md5(text).slice(0, 8);
+
+  cos.putObject(
+    {
+      Bucket: 'hostext-1254175213',
+      Region: 'ap-guangzhou',
+      Key: hash,
+      ContentType: 'text/plain',
+      CacheControl: 'max-age: 31536000',
+      Body: Buffer.from(text)
+    },
+    (err, data) => {
+      if (err) {
+        reply.code = 400;
+        reply.send(err);
+      } else {
+        reply.send(`curl -L https://text.cinwell.com/${hash}\n`);
+      }
+    }
+  );
+});
+
+fastify.get('/:hash', (req, reply) => {
+  const { hash } = req.params;
+  const url = `//hostext-1254175213.file.myqcloud.com/${hash}`;
+  reply.redirect(url);
+});
+
+fastify.listen(4930, '0.0.0.0', err => {
+  if (err) throw err;
+  console.log(`server listening on ${fastify.server.address().port}`);
+});
